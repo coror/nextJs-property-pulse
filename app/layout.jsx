f5c7aa9163863af5f1e7a0a96de380a947d53635
unknown <coralic.erin@gmail.com>
2024-05-27 00:45:15 +0200

    Initial Committ2

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,32 @@
-import '@/assets/styles/globals.css'
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import AuthProvider from '@/components/AuthProvider';
+import '@/assets/styles/globals.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { GlobalProvider } from '@/context/GlobalContext';
+import 'photoswipe/dist/photoswipe.css';
 
 export const metadata = {
-    title: 'PropertyPulse | Find The Perfect Rental',
-    description: 'Find your dream rental property',
-    keywords: 'rental, find rentals, find properties'
-} // this is for metadata. It will change for all the pages, but you can put separately to components
+  title: 'PropertyPulse | Find The Perfect Rental',
+  description: 'Find your dream rental property',
+  keywords: 'rental, find rentals, find properties',
+}; // this is for metadata. It will change for all the pages, but you can put separately to components
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang='en'>
-      <body>
-        <div>{children}</div>
-      </body>
-    </html>
+    <GlobalProvider>
+      <AuthProvider>
+        <html lang='en'>
+          <body>
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+            <ToastContainer />
+          </body>
+        </html>
+      </AuthProvider>
+    </GlobalProvider>
   );
 };
 
